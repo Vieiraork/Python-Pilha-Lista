f43892878758790e1b3d478ebcc96eb496435d4c
WilliamViei <vieiraork@gmail.com>
2020-10-09 12:16:42 -0300

    Acrescentado as opções FIFO e LIFO

--- Lista.py
+++ Lista.py
@@ -8,13 +8,14 @@ lista = [] * 10
 while True:
     print('''Comando válidos:      
     1 - Inserir
-    2 - Retirar
-    3 - Listar
-    4 - Sair''')
+    2 - Apagar o primeiro (FIFO)
+    3 - Apagar o ultimo (LIFO)
+    4 - Lista
+    5 - Sair''')
     print('-' * 30)
 
     op = int(input('Digite a opção: '))
-    while 4 > op < 1:
+    while 5 > op < 1:
         op = int(input('Digite a opção: '))
 
     print('-' * 30)
@@ -38,10 +39,18 @@ while True:
         print('-' * 30)
 
     elif op == 3:
+        if len(lista) < 1:
+            print('Lista vazia')
+        else:
+            print(f'O valor {lista[-1]} foi retirado da lista')
+            lista.pop()
+        print('-' * 30)
+
+    elif op == 4:
         for i in lista:
             print(f'{i} - ', end='')
         print('Fim da lista')
         print('-' * 30)
 
-    elif op == 4:
+    elif op == 5:
         break
